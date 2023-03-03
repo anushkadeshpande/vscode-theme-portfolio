4097fcbd8a3e7d928f6e5f3bc563a6d4d917c49a
anushkadeshpande <anushkaa5000@gmail.com>
2023-03-04 01:53:02 +0530

    style: fixed project list style

--- src/components/ProjectsList.js
+++ src/components/ProjectsList.js
@@ -1,37 +1,43 @@
 import { getDocs } from "firebase/firestore";
-import { useState, useEffect, createContext } from 'react'
-import './ProjectsList.css'
-import {projectsCollection} from '../firebase'
+import { useState, useEffect, createContext } from "react";
+import "./ProjectsList.css";
+import { projectsCollection } from "../firebase";
 
-import ProjectsListItem from './ProjectsListItem'
+import ProjectsListItem from "./ProjectsListItem";
 
 const ProjectsList = () => {
-    const [projects, setProjects] = useState([])
+  const [projects, setProjects] = useState([]);
 
-    useEffect(() => {
-            getDocs(projectsCollection)
-            .then((querySnapshot)=>{               
-                const newData = querySnapshot.docs
-                    .map((doc) => ({...doc.data(), id:doc.id }));  
-                    
-                    setProjects(newData)
-                // console.log(newData);
-            })
-    }, [])
+  useEffect(() => {
+    getDocs(projectsCollection).then((querySnapshot) => {
+      const newData = querySnapshot.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
 
-    return (
-        <div className="ProjectsList">
-            <div className="ProjectsList__title">PROJECTS</div>
-            <div className="ProjectsList__list">
-                <div className="ProjectsList__list__item">
-                    {
-                        projects?.map((data, key) => <ProjectsListItem data={data} key={key} onClick={(data) => {createContext(data)}}/>)
-                    }
-                </div>
-            </div>
+      setProjects(newData);
+      // console.log(newData);
+    });
+  }, []);
 
+  return (
+    <div className="ProjectsList">
+      <div className="ProjectsList__title">PROJECTS</div>
+      <div className="ProjectsList__list">
+        <div className="ProjectsList__list__item">
+          {projects?.map((data, key) => (
+            <ProjectsListItem
+              data={data}
+              key={key}
+              onClick={(data) => {
+                createContext(data);
+              }}
+            />
+          ))}
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default ProjectsList;
--- src/components/ProjectsListItem.js
+++ src/components/ProjectsListItem.js
@@ -1,22 +1,23 @@
-import {useContext } from 'react'
-import './ProjectsListItem.css'
-import Logo from '../assets/logo.svg'
+import { useContext } from "react";
+import "./ProjectsListItem.css";
+import Logo from "../assets/logo.svg";
 import ProjectContext from "../context/Project/ProjectContext";
 
-const ProjectsListItem = ({data}) => {
-
-    const projectData = useContext(ProjectContext)
-    return (
-        <div className="ProjectsListItem">
-            {console.log(data)}
-            <img src={Logo} />
-                    <div className="project_details">
-                        <h4>{data.Name}</h4>
-                        <h5>{data.Description}</h5>
-                    </div>
-                    <button onClick={() => projectData.updateState(data)}>Open</button>
+const ProjectsListItem = ({ data }) => {
+  const projectData = useContext(ProjectContext);
+  return (
+    <div className="ProjectsListItem">
+      <img src={data.logo} />
+      <span>
+        <div className="project_details">
+          <h4>{data.Name}</h4>
+          <h5>{data.Description}</h5>
         </div>
-    )
-}
 
-export default ProjectsListItem
+        <button onClick={() => projectData.updateState(data)}>Open</button>
+      </span>
+    </div>
+  );
+};
+
+export default ProjectsListItem;
