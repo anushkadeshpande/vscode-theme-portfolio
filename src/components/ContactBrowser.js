fec2d308563be02c8d74f3a40b1098e363101278
anushkadeshpande <anushkaa5000@gmail.com>
2026-04-01 21:25:48 +0530

    Make all contact form fields mandatory

--- src/components/ContactBrowser.js
+++ src/components/ContactBrowser.js
@@ -34,11 +34,11 @@ const ContactBrowser = () => {
         <div className="ContactBrowser__body__header">
           Have something to say? <span>Get in touch.</span>
         </div>
-        <div className="ContactBrowser__body__form">
-          <input type="text" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} />
-          <input type="text" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
-          <textarea placeholder="Message" value={message} onChange={(e) => setMessage(e.target.value)} />
-          <button onClick={() => submitForm()}>
+        <form className="ContactBrowser__body__form" onSubmit={(e) => { e.preventDefault(); submitForm(); }}>
+          <input type="text" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} required />
+          <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+          <textarea placeholder="Message" value={message} onChange={(e) => setMessage(e.target.value)} required />
+          <button type="submit">
             Send{" "}
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -49,7 +49,7 @@ const ContactBrowser = () => {
               <path d="M3.105 2.289a.75.75 0 00-.826.95l1.414 4.925A1.5 1.5 0 005.135 9.25h6.115a.75.75 0 010 1.5H5.135a1.5 1.5 0 00-1.442 1.086l-1.414 4.926a.75.75 0 00.826.95 28.896 28.896 0 0015.293-7.154.75.75 0 000-1.115A28.897 28.897 0 003.105 2.289z" />
             </svg>
           </button>
-        </div>
+        </form>
       </div>
     </div>
   );
