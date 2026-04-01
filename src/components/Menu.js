d45bffc567d32d5bcea5f3562fea2ea873579042
anushkadeshpande <anushkaa5000@gmail.com>
2026-04-01 21:28:56 +0530

    Remove external link from nav menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -3,7 +3,6 @@ import "./Menu.css";
 import { ReactComponent as Home } from "../assets/menu/Home.svg";
 import { ReactComponent as Profile } from "../assets/menu/Profile.svg";
 import { ReactComponent as Apps } from "../assets/menu/Apps.svg";
-import { ReactComponent as Other } from "../assets/menu/Other.svg";
 import { ReactComponent as Contact } from "../assets/menu/Contact.svg";
 import { Link, useLocation } from 'react-router-dom'
 
@@ -15,7 +14,6 @@ const Menu = () => {
       <Link to='/' className={location.pathname === "/"? "active" : ""}><Home /></Link>
       <Link to='/about' className={location.pathname === "/about"? "active" : ""}><Profile /></Link>
       <Link to='/projects' className={location.pathname === "/projects"? "active" : ""}><Apps /></Link>
-      <a href='https://annkive.web.app/' target="_blank" className={location.pathname === "/activities"? "active" : ""}><Other /></a>
       <Link to='/contact' className={location.pathname === "/contact"? "active" : ""}><Contact /></Link>
     </div>
   );
